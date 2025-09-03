005bd31a1bf66306a078927cd0964779ee7fb16d
Alexander Yurtaev <alexander.yurtaev@gmail.com>
2025-09-03 16:27:42 +0300

    Add implementations for list and update commands.

--- TaskTracker.Cli/Program.cs
+++ TaskTracker.Cli/Program.cs
@@ -27,31 +27,77 @@ public static class Program
             case "add":
                 await AddCommand(args, repository);
                 break;
+            case "list":
+                await ListCommand(args, repository);
+                break;
+            case "update":
+                await UpdateCommand(args, repository);
+                break;
             default:
                 Console.WriteLine("You write a wrong command!");
                 break;
         }
     }
 
-    private static async Task AddCommand(string[] commandLineParts, TaskRepository repository)
+    private static async Task AddCommand(string[] args, TaskRepository repository)
     {
-        if (commandLineParts.Length != 2)
+        if (args.Length != 2)
         {
             Console.WriteLine("You enter too much or less parameters for Add command.");
             Console.WriteLine("You need enter only Description for task.");
+            return;
         }
-        else
+
+        var description = args[1];
+        try
         {
-            var description = commandLineParts[1];
-            try
+            var id = await repository.Add(description);
+            Console.WriteLine($"Task added successfully (ID: {id})");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    private static async Task ListCommand(string[] args, TaskRepository repository)
+    {
+        try
+        {
+            var tasks = await repository.GetAllTasks();
+            Console.WriteLine(new String('-', 40));
+            Console.WriteLine($"|{nameof(Repository.Models.Task.Id), -3}|{nameof(Repository.Models.Task.Description), -25}|{nameof(Repository.Models.Task.Status), -8}|");
+            Console.WriteLine(new String('-', 40));
+            foreach (Repository.Models.Task task in tasks)
             {
-                var id = await repository.Add(description);
-                Console.WriteLine($"Task added successfully (ID: {id})");
+                Console.WriteLine($"|{task.Id, -3}|{task.Description, -25}|{task.Status, -8}|");
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            Console.WriteLine(new String('-', 40));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    private static async Task UpdateCommand(string[] args, TaskRepository repository)
+    {
+        if (args.Length != 3)
+        {
+            Console.WriteLine("You enter too much or less parameters for Add command.");
+            Console.WriteLine("You need enter task id and new Description for task.");
+            return;
+        }
+
+        var id = int.Parse(args[1]);
+        var description = args[2];
+        try
+        {
+            await repository.Update(id, description);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
         }
     }
 }
